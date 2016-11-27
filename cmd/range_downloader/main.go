cdce93e46ac33ea186511dd8d31941043db35068
Wenhui Shen <swhbox@foxmail.com>
2016-11-27 12:17:50 +0800

    update

---
+++ cmd/range_downloader/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/webx-top/webx/lib/com"
+
+	"flag"
+	"log"
+	nurl "net/url"
+	"strings"
+)
+
+func main() {
+	url := flag.String("url", "http://storage.googleapis.com/vimeo-test/work-at-vimeo.mp4", "URL for download")
+	threads := flag.Int("threads", 100, "Number of threads to download with")
+	flag.Parse()
+
+	u, err := nurl.Parse(*url)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	saveTo := strings.Replace(u.Path[1:], "/", "-", -1)
+	err = com.RangeDownload(*url, saveTo, *threads)
+	if err != nil {
+		log.Println(err)
+	}
+}
