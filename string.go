cdeb8f6b898fd322403b249454c16b55d4756aa9
Wenhui Shen <swhbox@foxmail.com>
2017-09-27 11:21:50 +0800

    update

--- string.go
+++ string.go
@@ -32,7 +32,18 @@ import (
 	"strings"
 	"time"
 	"unicode"
+	"unsafe"
 )
+
+func Str2bytes(s string) []byte {
+	x := (*[2]uintptr)(unsafe.Pointer(&s))
+	h := [3]uintptr{x[0], x[1], x[1]}
+	return *(*[]byte)(unsafe.Pointer(&h))
+}
+
+func Bytes2str(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
 
 // Md5 md5 hash string
 func Md5(str string) string {
