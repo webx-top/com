62c87cfc17366e2164767520a42f47eca9b6e803
Wenhui Shen <swhbox@foxmail.com>
2017-12-10 11:13:54 +0800

    update

--- time.go
+++ time.go
@@ -15,6 +15,7 @@
 package com
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
 	"strings"
@@ -283,4 +284,128 @@ var StartTime = time.Now()
 //TotalRunTime 总运行时长
 func TotalRunTime() string {
 	return FriendlyTime(time.Now().Sub(StartTime))
+}
+
+var (
+	units     = []string{"years", "weeks", "days", "hours", "minutes", "seconds", "milliseconds"}
+	unitsZhCN = map[string]string{"years": "年", "weeks": "周", "days": "天", "hours": "小时", "minutes": "分", "seconds": "秒", "milliseconds": "毫秒"}
+)
+
+// Durafmt holds the parsed duration and the original input duration.
+type Durafmt struct {
+	duration time.Duration
+	input    string // Used as reference.
+	units    map[string]string
+}
+
+func getDurationUnits(args []interface{}) map[string]string {
+	var units map[string]string
+	if len(args) > 0 {
+		switch v := args[0].(type) {
+		case map[string]string:
+			units = v
+		case string:
+			switch v {
+			case `zh_CN`, `zh-CN`:
+				units = unitsZhCN
+			}
+		}
+	}
+	return units
+}
+
+// ParseDuration creates a new *Durafmt struct, returns error if input is invalid.
+func ParseDuration(dinput time.Duration, args ...interface{}) *Durafmt {
+	input := dinput.String()
+	return &Durafmt{dinput, input, getDurationUnits(args)}
+}
+
+// ParseDurationString creates a new *Durafmt struct from a string.
+// returns an error if input is invalid.
+func ParseDurationString(input string, args ...interface{}) (*Durafmt, error) {
+	if input == "0" || input == "-0" {
+		return nil, errors.New("durafmt: missing unit in duration " + input)
+	}
+	duration, err := time.ParseDuration(input)
+	if err != nil {
+		return nil, err
+	}
+	return &Durafmt{duration, input, getDurationUnits(args)}, nil
+}
+
+// String parses d *Durafmt into a human readable duration.
+func (d *Durafmt) String() string {
+	var duration string
+
+	// Check for minus durations.
+	if string(d.input[0]) == "-" {
+		duration += "-"
+		d.duration = -d.duration
+	}
+
+	// Convert duration.
+	seconds := int64(d.duration.Seconds()) % 60
+	minutes := int64(d.duration.Minutes()) % 60
+	hours := int64(d.duration.Hours()) % 24
+	days := int64(d.duration/(24*time.Hour)) % 365 % 7
+	weeks := int64(d.duration/(24*time.Hour)) / 7 % 52
+	years := int64(d.duration/(24*time.Hour)) / 365
+	milliseconds := int64(d.duration/time.Millisecond) -
+		(seconds * 1000) - (minutes * 60000) - (hours * 3600000) -
+		(days * 86400000) - (weeks * 604800000) - (years * 31536000000)
+
+	// Create a map of the converted duration time.
+	durationMap := map[string]int64{
+		"milliseconds": milliseconds,
+		"seconds":      seconds,
+		"minutes":      minutes,
+		"hours":        hours,
+		"days":         days,
+		"weeks":        weeks,
+		"years":        years,
+	}
+
+	// Construct duration string.
+	for _, u := range units {
+		v := durationMap[u]
+		if customLable, ok := d.units[u]; ok {
+			u = customLable
+		}
+		strval := strconv.FormatInt(v, 10)
+		switch {
+		// add to the duration string if v > 1.
+		case v > 1:
+			duration += strval + " " + u + " "
+		// remove the plural 's', if v is 1.
+		case v == 1:
+			duration += strval + " " + strings.TrimRight(u, "s") + " "
+		// omit any value with 0s or 0.
+		case d.duration.String() == "0" || d.duration.String() == "0s":
+			// note: milliseconds and minutes have the same suffix (m)
+			// so we have to check if the units match with the suffix.
+
+			// check for a suffix that is NOT the milliseconds suffix.
+			if strings.HasSuffix(d.input, string(u[0])) && !strings.Contains(d.input, "ms") {
+				// if it happens that the units are milliseconds, skip.
+				if u == "milliseconds" {
+					continue
+				}
+				duration += strval + " " + u
+			}
+			// process milliseconds here.
+			if u == "milliseconds" {
+				if strings.Contains(d.input, "ms") {
+					duration += strval + " " + u
+					break
+				}
+			}
+			break
+		// omit any value with 0.
+		case v == 0:
+			continue
+		}
+	}
+	// trim any remaining spaces.
+	duration = strings.TrimSpace(duration)
+	return duration
 }
