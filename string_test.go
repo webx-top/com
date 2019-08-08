14a95fb6dce19e9e5d05feffd26676070be13abe
shen_wen_hui <shenwenhui@zuzuche.com>
2019-08-08 16:57:08 +0800

    improved

--- string_test.go
+++ string_test.go
@@ -10,3 +10,11 @@ func TestTitleCase(t *testing.T) {
 	assert.Equal(t, `Webx_Top`, TitleCase(`webx_top`))
 	assert.Equal(t, `Webx Top`, TitleCase(`webx top`))
 }
+
+func TestSafeBase64(t *testing.T) {
+	s, e := SafeBase64Decode(SafeBase64Encode(`webx_top`))
+	if e != nil {
+		panic(e)
+	}
+	assert.Equal(t, `webx_top`, s)
+}
