edfde821770e47e2cf319f17f414c920bbf27a49
Hank Shen <swhbox@foxmail.com>
2019-10-03 13:43:50 +0800

    update

---
+++ decimal_test.go
@@ -0,0 +1,28 @@
+package com
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNumberTrim(t *testing.T) {
+	s := `2.123987666`
+	r := NumberTrim(s, 5)
+	assert.Equal(t, `2.12398`, r)
+	r = NumberTrim(s, 1, ``)
+	assert.Equal(t, `2.1`, r)
+}
+
+func TestNumberTrimZero(t *testing.T) {
+	s := fmt.Sprintf("%.7f", 2.123)
+	assert.Equal(t, `2.1230000`, s)
+	assert.Equal(t, 2.123, Float64(s))
+
+	r := NumberTrimZero(s)
+	assert.Equal(t, `2.123`, r)
+
+	r = FormatBytes(12344566, 2, true)
+	assert.Equal(t, `11.77MB`, r)
+}
