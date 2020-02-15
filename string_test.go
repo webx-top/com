7a28fa9f8d28a0684e3f770f7a69f7d28339b594
Hank Shen <swhbox@foxmail.com>
2020-02-15 17:45:51 +0800

    update

--- string_test.go
+++ string_test.go
@@ -11,6 +11,13 @@ func TestTitleCase(t *testing.T) {
 	assert.Equal(t, `Webx Top`, TitleCase(`webx top`))
 }
 
+func TestSlashes(t *testing.T) {
+	assert.Equal(t, `webx\'top\\`, AddSlashes(`webx'top\`))
+	assert.Equal(t, `webx'top\`, StripSlashes(`webx\'top\\`))
+	assert.Equal(t, `webx\\\'top\\\\`, AddSlashes(`webx\'top\\`))
+	assert.Equal(t, `webx\'top\\`, StripSlashes(`webx\\\'top\\\\`))
+}
+
 func TestSafeBase64(t *testing.T) {
 	s, e := SafeBase64Decode(SafeBase64Encode(`webx_top`))
 	if e != nil {
