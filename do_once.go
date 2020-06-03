4837ba83308661ef260a16603a5f5345bd505214
Hank Shen <swh@admpub.com>
2020-06-03 17:13:58 +0800

    improved

--- do_once.go
+++ do_once.go
@@ -6,19 +6,17 @@ import (
 
 type DoOnce struct {
 	lock sync.RWMutex
-	data SyncWaitGroup
+	data map[interface{}]*sync.WaitGroup
 }
 
-type SyncWaitGroup map[interface{}]*sync.WaitGroup
-
-// Req 同一时刻只有一个请求能获取执行权限，获得执行权限的线程接下来需要执行具体的业务逻辑，完成后调用release方法通知其他线程，操作完成，获取资源即可，其他请求接下来需要调用wait方法
+// CanSet 同一时刻只有一个请求能获取执行权限，获得执行权限的线程接下来需要执行具体的业务逻辑，完成后调用release方法通知其他线程，操作完成，获取资源即可，其他请求接下来需要调用wait方法
 // reqTag 请求标识 用于标识同一个资源
-func (u *DoOnce) Req(reqTag interface{}) bool {
+func (u *DoOnce) CanSet(reqTag interface{}) bool {
 	u.lock.Lock()
 	defer u.lock.Unlock()
 
 	if u.data == nil {
-		u.data = make(SyncWaitGroup)
+		u.data = map[interface{}]*sync.WaitGroup{}
 	}
 
 	_, ok := u.data[reqTag]
