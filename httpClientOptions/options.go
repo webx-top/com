4b6ddfacbee6af0dac75e93f1216cf11b3f51a11
Hank Shen <swhbox@foxmail.com>
2020-07-02 11:56:39 +0800

    update

--- httpClientOptions/options.go
+++ httpClientOptions/options.go
@@ -11,7 +11,7 @@ import (
 	"github.com/webx-top/com"
 )
 
-func ApplyOptions(client *http.Client, options ...com.HTTPClientOptions) *http.Client {
+func Apply(client *http.Client, options ...com.HTTPClientOptions) *http.Client {
 	for _, option := range options {
 		option(client)
 	}
