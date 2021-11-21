c7b5c79c61b2484f76caad47d507c48a44ccf2fd
Hank Shen <swh@admpub.com>
2021-11-21 15:26:10 +0800

    update

---
+++ url_test.go
@@ -0,0 +1,35 @@
+package com
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSplitHostPort(t *testing.T) {
+	host, port := SplitHostPort(`[1:2:2:3]:9999`)
+	assert.Equal(t, `[1:2:2:3]`, host)
+	assert.Equal(t, `9999`, port)
+
+	host, port = SplitHostPort(`example.com:9999`)
+	assert.Equal(t, `example.com`, host)
+	assert.Equal(t, `9999`, port)
+
+	host, port = SplitHostPort(`127.0.0.1:9999`)
+	assert.Equal(t, `127.0.0.1`, host)
+	assert.Equal(t, `9999`, port)
+}
+
+func TestSplitHostPort2(t *testing.T) {
+	host, port := SplitHostPort(`[1:2:2:3]`)
+	assert.Equal(t, `[1:2:2:3]`, host)
+	assert.Equal(t, ``, port)
+
+	host, port = SplitHostPort(`example.com`)
+	assert.Equal(t, `example.com`, host)
+	assert.Equal(t, ``, port)
+
+	host, port = SplitHostPort(`127.0.0.1`)
+	assert.Equal(t, `127.0.0.1`, host)
+	assert.Equal(t, ``, port)
+}
