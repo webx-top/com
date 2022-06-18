bca76362aa801a8d24aca7b60c14428c56600382
Hank Shen <swh@admpub.com>
2022-06-18 10:07:10 +0800

    update

--- goroutine_test.go
+++ goroutine_test.go
@@ -3,6 +3,7 @@ package com
 import (
 	"context"
 	"fmt"
+	"sync"
 	"testing"
 	"time"
 )
@@ -15,4 +16,23 @@ func TestLoop(t *testing.T) {
 	}, time.Second*1)
 	time.Sleep(time.Second * 5)
 	cancel()
+}
+
+func TestDelayOnce(t *testing.T) {
+	d := NewDelayOnce(time.Second*2, time.Hour)
+	ctx := context.TODO()
+	wg := sync.WaitGroup{}
+	for i := 0; i < 10; i++ {
+		fmt.Println(`Trigger`, time.Now())
+		isNew := d.Do(ctx, `key`, func() error {
+			defer wg.Done()
+			fmt.Println(`Execute`, time.Now())
+			return nil
+		})
+		if isNew {
+			wg.Add(1)
+		}
+		time.Sleep(time.Second * 2)
+	}
+	wg.Wait()
 }
