564f565bd5362620199f42315dd9200ce6b54e57
Hank Shen <swh@admpub.com>
2023-10-18 14:29:04 +0800

    update

--- cmd_test.go
+++ cmd_test.go
@@ -1,7 +1,11 @@
 package com
 
 import (
+	"bufio"
+	"context"
+	"io"
 	"os"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -17,4 +21,38 @@ func TestParseWindowsEnvVar(t *testing.T) {
 	os.Setenv(`TESTENV`, `2`)
 	v := ParseWindowsEnvVar(`ab{%TESTENV%}c`)
 	assert.Equal(t, `ab2c`, v)
+}
+
+func TestCmdChanReader(t *testing.T) {
+	c := NewCmdChanReader()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer func() {
+		cancel()
+		c.Close()
+	}()
+	wg := sync.WaitGroup{}
+	wg.Add(5)
+	go func() {
+		in := bufio.NewReader(c)
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				input, err := in.ReadString(LF)
+				if err != nil && err != io.EOF {
+					t.Log(err.Error())
+					return
+				}
+				wg.Done()
+				t.Log(input)
+			}
+		}
+	}()
+	c.SendStringAndWait("OK\n")
+	c.SendStringAndWait("OK1\n")
+	c.SendStringAndWait("OK2\n")
+	c.SendStringAndWait("OK3\n")
+	c.SendStringAndWait("OK4\n")
+	wg.Wait()
 }
